626836af7c20f78bd0698ebdb807ba7255d6bc74
davidruncode <daguz.91@gmail.com>
2024-09-05 14:19:37 -0400

    Cambio de HTML a ejs

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 const express = require('express');
+const path = require('path');
+
+const indexRouter = require('./src/routes/index');
+
 const app = express();
-const userRoutes = require('./routes/user');
 
-// Middleware para servir archivos estáticos
-app.use(express.static('public'));
+// Configura la carpeta 'public' para archivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Usar las rutas definidas
-app.use(userRoutes);
+// view engine setup
+// Configura EJS como el motor de vistas
+app.set('views', path.join(__dirname, 'src/views'));
+app.set('view engine', 'ejs');
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+// Especifica la carpeta donde están las vistas
+app.set('views', './src/views');
+
+// Rutas
+app.use('/', indexRouter);
+
+//Server
+app.listen(3000, () => {
+    console.log('Servidor en el puerto 3000');
 });
+
+module.exports = app;
